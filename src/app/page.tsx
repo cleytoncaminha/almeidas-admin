8d9a96a5161bd77bb2e2c4646020dac41d265d86
Cleyton Caminha <cleytoncaminha01@gmail.com>
2025-03-26 21:00:15 -0300

    adjust styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ import ModalOne from "@/components/ModalOne";
 import ModalTwo from "@/components/ModalTwo";
 import ModalThree from "@/components/ModalThree";
 import { Box } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
+import AddHomeIcon from '@mui/icons-material/AddHome';
+import Image from "next/image";
 
 const Home: React.FC = () => {
   const [modal, setModal] = useState<number | null>(null);
@@ -13,19 +17,41 @@ const Home: React.FC = () => {
     <Box 
         display="flex" 
         alignItems="center" 
-        justifyContent="space-between"
-        height="90vh"
-        width={{ xs: "100%", sm: "70%" }}
+        height="100vh"
+        width={{ xs: "100%", sm: "100%" }}
         margin="auto"
         flexDirection={{ xs: "column", sm: "row" }}
-        gap={2} 
-        padding={{ xs: "20px", sm: "0" }}
+        justifyContent={{ xs: "flex-end", sm: "space-around" }}
+        gap={{ xs: 6, sm: 2 }} 
+        padding={{ xs: "30px", sm: "0" }}
+        sx={{
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        }}
     >
-      <Button label="Abrir Modal 1" onClick={() => { 
+            <Box
+        sx={{
+          display: { xs: 'block', sm: 'none' }, // Mostra a imagem apenas em telas pequenas
+          position: 'absolute',
+          top: '2%', 
+          left: '50%',
+          transform: 'translateX(-50%)',
+          width: '100%',
+          maxWidth: '300px', // Ajuste o tamanho conforme necessário
+        }}
+      >
+        <Image
+          src="/logo_almeidas.png"
+          alt="Logo Almeidas"
+          width={300}
+          height={150}
+          style={{ width: '100%', height: 'auto' }}
+        />
+      </Box>
+      <Button label="Buscar Código" onClick={() => { 
         setModal(1)}
-        } />
-      <Button label="Abrir Modal 2" onClick={() => setModal(2)} />
-      <Button label="Abrir Modal 3" onClick={() => setModal(3)} />
+        } icon={<SearchIcon fontSize="inherit" />} />
+      <Button label="Solicitar Serviço" onClick={() => setModal(2)} icon={<MiscellaneousServicesIcon fontSize="inherit" />} />
+      <Button label="Solicitar Retirada" onClick={() => setModal(3)} icon={<AddHomeIcon fontSize="inherit" />} />
 
       <ModalOne isOpen={modal === 1} onClose={() => setModal(null)} />
       <ModalTwo isOpen={modal === 2} onClose={() => setModal(null)} />
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,37 @@
 import Button from "@mui/material/Button";
+import { ReactNode } from "react";
 
 type ButtonProps = {
   label: string;
   onClick: () => void;
+  icon?: ReactNode;
 };
 
-const CustomButton: React.FC<ButtonProps> = ({ label, onClick }) => {
+const CustomButton: React.FC<ButtonProps> = ({ label, onClick, icon }) => {
   return (
-    <Button variant="contained" color="primary" onClick={onClick} sx={{ width: 200, height: 50 }}>
-      {label}
+    <Button
+      variant="contained"
+      onClick={onClick}
+      sx={{
+        width: { xs: '50vw', sm: '200px' },
+        height: { xs: '20vh', sm: '200px' },
+        borderRadius: "20px",
+        backgroundColor: "grey.500",
+        color: "black",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.2)",
+        transition: "all 0.2s ease-in-out",
+        "&:hover": {
+          backgroundColor: "grey.700",
+          boxShadow: "6px 6px 15px rgba(0, 0, 0, 0.3)",
+        },
+      }}
+    >
+      {icon && <span style={{ fontSize: 70 }}>{icon}</span>}
+      <span style={{fontWeight: "bold", marginTop: -10}}>{label}</span>
     </Button>
   );
 };
